4b0728a0a05bef87459c385ed986d2db5485b85d
mori5321 <moriwm77@gmail.com>
2021-01-16 13:39:22 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,11 @@ import "assets/stylesheets/reset.css";
 import "tailwindcss/tailwind.css";
 import "assets/stylesheets/global.css";
 import "assets/stylesheets/markdown.css";
-import Head from 'next/Head';
 import { Header } from '@/components/modules/Header';
 
 const MossApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-        <Head>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
         <div className="max-w-4xl mx-auto continer">
           <Header />
           <div className="pt-14" />
