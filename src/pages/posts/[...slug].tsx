b82cb4af7dfe2aad3d0d0e72a91915065a048cc5
mori5321 <moriwm77@gmail.com>
2021-07-06 23:11:58 +0900

    update

--- src/pages/posts/[...slug].tsx
+++ src/pages/posts/[...slug].tsx
@@ -1,83 +1,86 @@
-import * as React from 'react';
-import { GetStaticProps, NextPage } from 'next';
-import { useRouter } from 'next/dist/client/router';
-import { getPostBySlug, getAllPostPaths, markdownToHtml, Post } from '@/utils/posts';
-import Prism from 'prismjs';
-
+import * as React from "react";
+import { GetStaticProps, NextPage } from "next";
+import { useRouter } from "next/dist/client/router";
+import {
+  getPostBySlug,
+  getAllPostPaths,
+  markdownToHtml,
+  Post,
+} from "@/utils/posts";
+import Prism from "prismjs";
 
 type Props = {
-  post: Post,
-  markdownHtml: string 
-}
+  post: Post;
+  markdownHtml: string;
+};
 
 const PostDetailPage: NextPage<Props> = ({ post, markdownHtml }) => {
   const router = useRouter();
 
   React.useEffect(() => {
     Prism.highlightAll();
-  }, [])
+  }, []);
 
-  if (router.isFallback)
-    return <div>Loading...</div>
+  if (router.isFallback) return <div>Loading...</div>;
 
   return (
     <div className="p-4 pb-24">
       <div className="">
-        <h1 className="text-3xl font-bold">
-          {post.metaData.title}
-        </h1>
+        <h1 className="text-3xl font-bold">{post.metaData.title}</h1>
         <p className="py-1 text-gray-400">{post.metaData.date}</p>
         <div className="">
-          {post.metaData.tags.map(tag => {
+          {post.metaData.tags.map((tag) => {
             return (
               <div
                 key={`${post.metaData.title}-${tag}`}
-                className="inline-block px-2 mr-1 bg-gray-200 rounded-full">
-                  {tag}
+                className="inline-block px-2 mr-1 bg-gray-200 rounded-full"
+              >
+                {tag}
               </div>
-            )
+            );
           })}
         </div>
       </div>
       <div
         className="py-8 markdown-field"
-        dangerouslySetInnerHTML={{ __html: markdownHtml }}>
-      </div>
+        dangerouslySetInnerHTML={{ __html: markdownHtml }}
+      ></div>
     </div>
-  )
-}
+  );
+};
 
 type Params = {
   params: {
-    slug: string[]
-  }
-}
-export const getStaticProps: GetStaticProps<Props> = async({params}: Params) => {
-  const post = getPostBySlug(params.slug)
+    slug: string[];
+  };
+};
+export const getStaticProps: GetStaticProps<Props> = async ({
+  params,
+}: Params) => {
+  const post = getPostBySlug(params.slug);
   const html = await markdownToHtml(post.markdown);
 
   return {
     props: {
       post: post,
       markdownHtml: html,
-    }
-  }
-}
+    },
+  };
+};
 
 // TODO: StaticPathsとStaticPropsの間をTypeSafeにしたい
-export const getStaticPaths = async() => {
-  const posts = getAllPostPaths()
+export const getStaticPaths = async () => {
+  const posts = getAllPostPaths();
   return {
-    paths: posts.map(post => {
+    paths: posts.map((post) => {
       return {
         params: {
-          slug: post
-        }
-      }
+          slug: post,
+        },
+      };
     }),
-    fallback: false
-  }
-}
+    fallback: false,
+  };
+};
 
-export default PostDetailPage
-
+export default PostDetailPage;
